16a0ec2b8c8e24daca67514ad1a05d1c7b6a94c5
Illia25 <steam8837@gmail.com>
2024-01-08 20:46:52 +0200

    Exceptions Lesson 3 All Tasks

---
+++ 26/2023/main.py
@@ -0,0 +1,64 @@
+# print("Hello World!")
+# print()
+# # print('test')
+# print('qwerty')
+# Завдання 1
+# Зчитуємо три цифри з клавіатури
+# number1 = int(input("Введіть число: "))
+# number2 = int(input("Введіть число: "))
+# number3 = int(input("Введіть число: "))
+
+# Знаходимо суму чисел
+# sum_result = number1 + number2 + number3
+
+# Знаходимо добуток чисел
+# product_result = number1 * number2 * number3
+
+# Виводимо результати на екран
+# print(f"Сума чисел: {sum_result}")
+# print(f"Добуток чисел: {product_result}")
+
+# Завдання 2
+# d1 AC = 14
+# d2 BD = 20
+# Задані довжини діагоналей
+# diagonal_AC = 14
+# diagonal_BD = 20
+
+# Обчислюємо площу ромба за формулою: S = (AC * BD) / 2
+# area_of_rhombus = (diagonal_AC * diagonal_BD) / 2
+
+# Виводимо результат на екран
+# print(f"Площа ромба з діагоналями {diagonal_AC} та {diagonal_BD} дорівнює {area_of_rhombus}")
+
+# завдання 3
+# number = int(input("Enter 4-digit number:"))
+
+# n1 = number // 100
+#
+# n2 = number // 10 % 10
+#
+# n2 = number % 100 // 10
+# n3 = number % 10
+#
+# result = n1 + n2 + n3
+
+# Зчитуємо число з чотирьох цифр з клавіатури
+# user_input = input("Введіть число з чотирьох цифр: ")
+
+# Перевірка на введення правильної кількості цифр
+# if len(user_input) != 4 or not user_input.isdigit():
+#     print("Будь ласка, введіть коректне число з чотирьох цифр.")
+# else:
+# Конвертуємо рядок у список цифр та обчислюємо добуток
+#     digits = [int(digit) for digit in user_input]
+#     product = 1
+#
+#     for digit in digits:
+#         product *= digit
+
+# Виводимо результат на екран
+#     print(f"Добуток цифр у введеному числі {user_input} дорівнює {product}")
+
+
+
